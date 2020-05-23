bf69a6740c53dab26356eb9cafe0f14356063093
edwin-do <doedwin123@gmail.com>
2020-05-23 18:21:47 -0400

    install got for http

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
+var got = require("got");
 
 var port = process.env.PORT || 8080;
 
@@ -18,6 +19,10 @@ app.get("/", function(req,res){
 app.get("/home", function(req,res){
     res.render("index");
 });
+
+app.get("/results", function(req,res){
+    res.render("results");
+})
 
 app.listen(port, function(){
     // console.log(process.env.PORT);
